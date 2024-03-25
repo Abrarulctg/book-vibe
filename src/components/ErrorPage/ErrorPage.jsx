bc99b20b0229cdfad0cebd0363cfd5eacf13a6a6
Abrarulctg <jubairctg99@gmail.com>
2024-03-26 01:54:22 +0600

    initialized project with basic setup

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className="text-6xl">Opss!</h1>
+            <h2 className="text-5xl">404</h2>
+            <h2 className="text-4xl">Page Not Found!</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import ListedBooks from './components/ListedBooks/ListedBooks.jsx';
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
+import PageToRead from './components/PageToRead/PageToRead.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: '/listed-books',
+        element: <ListedBooks></ListedBooks>
+      },
+      {
+        path: '/page-to-read',
+        element: <PageToRead></PageToRead>
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
